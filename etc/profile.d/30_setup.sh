1816e4d6533585f2129b34978eb8c402afaae93b
Patrick Schleizer <adrelanos@riseup.net>
2014-12-13 16:46:53 +0000

    refactoring

--- etc/profile.d/30_setup.sh
+++ etc/profile.d/30_setup.sh
@@ -9,5 +9,5 @@ if [ ! "$(tty)" = "/dev/tty1" ]; then
 else
    ## Not launching into background using &, because we need to listen for
    ## STRG + C (signal sigterm).
-   /usr/lib/whonixsetup-maybe-start
+   /usr/lib/whonixsetup_cli_start_maybe
 fi
